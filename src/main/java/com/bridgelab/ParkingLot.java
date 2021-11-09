50bb5749ac574bcb2e4240ca491b87f01a603222
asimbinahammed <asim242370@gmail.com>
2021-11-09 16:49:48 +0530

    Added: Welcome message

--- src/main/java/com/bridgelab/ParkingLot.java
+++ src/main/java/com/bridgelab/ParkingLot.java
@@ -1,5 +1,10 @@
 package com.bridgelab;
 
+/**
+ * Purpose : To check availability o slot for parking
+ * @author : ASIM AHAMMED
+ * @since : 09-11-2021
+ */
 public class ParkingLot {
     public static void main(String[] args) {
         System.out.println("Welcome to Parking Lot");
